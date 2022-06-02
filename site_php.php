ae07e78b06fed3282f8c08f0d42792441b61e783
Emanuel Souza <emanuelsouza.1987@gmail.com>
2022-06-01 21:24:23 -0300

    Update site_php.php

--- site_php.php
+++ site_php.php
@@ -7,7 +7,7 @@
     <body>
         <?php
              for ($i = 0 ; $i < 10  ; $i++) {
-                print ("Linha número " .$i. "<br/>");
+                print ("Linha número " .$i. " <br/>");
              }
         ?>
     </body>
